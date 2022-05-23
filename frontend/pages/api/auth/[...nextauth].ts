19715363acdb48890e23a97194ef8a447ea2b1cf
Hyeokjun seo <75557859+ANTARES-KOR@users.noreply.github.com>
2022-05-23 10:13:23 +0900

    chore: setup frontend repo (#1)

---
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth/next";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+    }),
+  ],
+});
