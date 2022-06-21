b726b6525f7572404b43d6ba36f38446282360ef
ddo di <eunddodi@ddos-MacBook-Air.local>
2022-06-21 22:15:11 +0900

    initial commit

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -8,4 +8,18 @@ export default NextAuth({
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
