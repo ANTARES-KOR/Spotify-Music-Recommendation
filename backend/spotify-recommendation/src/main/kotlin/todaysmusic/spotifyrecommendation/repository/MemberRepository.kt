61c73540f039ff3661e9af619abdcea82dcb8d5f
jwjin95 <jwjin95@yonsei.ac.kr>
2022-06-23 19:34:43 +0900

    sync: sync

--- backend/spotify-recommendation/src/main/kotlin/todaysmusic/spotifyrecommendation/repository/MemberRepository.kt
+++ backend/spotify-recommendation/src/main/kotlin/todaysmusic/spotifyrecommendation/repository/MemberRepository.kt
@@ -1,14 +1,10 @@
 package todaysmusic.spotifyrecommendation.repository
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import todaysmusic.spotifyrecommendation.domain.Member
-import javax.persistence.EntityManager
 
 @Repository
-interface MemberRepository: CrudRepository<Member, Int> {
-
-    fun findMemberById(id: Int): Member?
-
-    fun findMemberByAccessToken(access_token: String): Member?
+interface MemberRepository: JpaRepository<Member, Int> {
+    fun findMemberByAccessToken() : Member?
 }
